3d757670c45af3b3bcab04994afe43bf827f48e9
Jase Thomson <jasethomson31@gmail.com>
2019-07-17 11:58:42 -0700

    v.02 gameboard

--- g5_magicmaze/jscomponents/main.js
+++ g5_magicmaze/jscomponents/main.js
@@ -1,17 +1,16 @@
 $(document).ready(initializeApp);
 
 var game = null;
+
 function initializeApp() {
   var magicMazeConfig = {
     height: 3,
     width: 3,
-    // frogCounts: {
-    //   'yellow': 10,
-    //   'red': 11,
-    //   'blue': 14,
-    //   'brown': 21
-    // }
   }
-  game = new MagicMaze(magicMazeConfig);
+  var pawnConfig = {
+    height: 4,
+    width: 4
+  }
+  game = new MagicMaze(magicMazeConfig, pawnConfig);
   game.makeGameBoard();
 }
